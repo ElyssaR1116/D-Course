4e39d97986e0b248ae5aeab8782f0e4e81877c26
Kristianto Haryodi <34808653+KrisHaryodi@users.noreply.github.com>
2020-11-23 23:59:40 +0700

    update

--- frontend/scripts/function.js
+++ frontend/scripts/function.js
@@ -20,3 +20,21 @@ function openInfo(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  function myMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.menu')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
